6cf604e7290715c4bdcae77ae86fa7adc471146d
Renato Nascimento <renatonascimento.dev@gmail.com>
2022-09-20 19:39:11 -0300

    Criação da estrutura de pastas

---
+++ Controllers/UserController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Registro_de_Ponto_CTEDS.Controllers
+{
+    public class UserController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
